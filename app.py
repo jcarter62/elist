b4fe6bce175fc7e735689a38e743e46bc1200c3d
jcarter <Running-97-miles>
2021-10-08 17:47:05 -0700

    several changes

--- app.py
+++ app.py
@@ -1,11 +1,70 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+from flask_bootstrap import Bootstrap
+from db import DB
 
 app = Flask(__name__)
+Bootstrap(app)
 
+@app.before_request
+def load_config():
+    app.config.from_object('config')
+    return
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def route_home():
+    db = DB()
+    employees = db.load_employees()
+    context = {
+        'employees': employees,
+        'title': 'Employees - Home'
+    }
+    return render_template('home.html', context=context)
+
+@app.route('/employee/<id>')
+def route_employee(id):
+    db = DB()
+    employee = db.load_employee(id)
+    context = {
+        'employee': employee,
+        'title': 'Display Employee'
+    }
+    return render_template('employee.html', context=context)
+
+@app.route('/edit/<id>')
+def route_edit_id(id):
+    db = DB()
+    employee = db.load_employee(id)
+    context = {
+        'employee': employee,
+        'title': 'Add Employee'
+    }
+    return render_template('editemployee.html', context=context)
+
+@app.route('/save', methods=['POST'])
+def route_save_post():
+    id = request.form['id']
+    db = DB()
+    emp = db.load_employee(id)
+    emp['first_name'] = request.form['i_first_name']
+    emp['last_name'] = request.form['i_last_name']
+    emp['desk_phone'] = request.form['i_desk_phone']
+    emp['mobile_phone'] = request.form['i_mobile_phone']
+    emp['email'] = request.form['i_email']
+    emp['title'] = request.form['i_title']
+    emp['empid'] = request.form['i_empid']
+    emp['location'] = request.form['i_location']
+    db.update_employee(emp)
+
+    url = '/edit/' + str(id)
+    return redirect(url)
+
+
+@app.route('/add')
+def route_add():
+    db = DB()
+    id = db.create_new_employee()
+    url = '/edit/' + str(id)
+    return redirect(url)
 
 
 if __name__ == '__main__':
---
+++ db/db.py
@@ -0,0 +1,210 @@
+import sqlite3
+from flask import current_app
+from sqlite3 import Error
+
+
+class DB:
+    # ref: https://www.sqlitetutorial.net/sqlite-python/create-tables/
+    _curapp = None
+    _dbpath = None
+    _conn = None
+    _maxid = 0
+
+    def __init__(self):
+        self._curapp = current_app
+        self._get_settings()
+        self._conn = self._create_connection()
+        self._create_employees_table()
+        if self._employee_records() <= 0:
+            self._create_empty_employees()
+            newid = self._get_next_id()
+            self.insert_employee(id=newid, first_name='Joe', last_name='User')
+        return
+
+    def _commit(self):
+        if self._conn is not None:
+            self._conn.commit()
+        return
+
+    def _get_settings(self):
+        self._dbpath = self._curapp.config['DATABASE_PATH']
+
+    def _create_connection(self):
+        """ create a database connection to the SQLite database
+            specified by db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(self._dbpath)
+            return conn
+        except Error as e:
+            print(e)
+        return conn
+
+    def _create_employees_table(self):
+        try:
+            c = self._conn.cursor()
+            cmd = """CREATE TABLE IF NOT EXISTS employees (
+                        id integer PRIMARY KEY,
+                        first_name text NOT NULL,
+                        last_name text NOT NULL,
+                        desk_phone text,
+                        mobile_phone text,
+                        email text,
+                        title text,
+                        empid text,
+                        location text,
+                        start_date text,
+                        end_date text,
+                        image text
+                    );"""
+            c.execute(cmd)
+        except Error as e:
+            print(e)
+        return
+
+    def _employee_records(self):
+        # ref https://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
+        num = 0
+        try:
+            c = self._conn.cursor()
+            cmd = 'Select count(*) from employees;'
+            c = c.execute(cmd)
+            rows = c.fetchall()
+
+            for row in rows:
+                num = num + row[0]
+        except Error as e:
+            print(e)
+        return num
+
+    def _get_next_id(self):
+        id = 0
+        try:
+            c = self._conn.cursor()
+            cmd = 'select max(id) from employees;'
+            c = c.execute(cmd)
+            row = c.fetchone()
+            if row is None:
+                id = 0
+            else:
+                id = row[0]
+        except Error as e:
+            print(e)
+        id = id + 1
+        return id
+
+    def _create_empty_employees(self):
+        try:
+            c = self._conn.cursor()
+            newid = self._get_next_id()
+            cmd1 = 'insert into employees (id, first_name, last_name) values (newid, "first", "last");'
+            cmd2 = 'delete from employees where id = newid;'
+            c.execute(cmd1)
+            c.execute(cmd2)
+            self._commit()
+
+        except Error as e:
+            print(e)
+        return
+
+    def insert_employee(self, id, first_name='', last_name='', desk_phone='', mobile_phone='', email='', title='',
+                        empid='', location='', start_date='', end_date='', image=''):
+        try:
+            cmd = 'insert into employees ' +\
+                  '(id, first_name, last_name, desk_phone, mobile_phone, email, title, empid, location, start_date, end_date, image) ' + \
+                  'values ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+            c = self._conn.cursor()
+            params = (id, first_name, last_name, desk_phone, mobile_phone,
+                      email, title, empid, location, start_date, end_date, image)
+            c.execute(cmd, params)
+            self._commit()
+        except Error as e:
+            print(e)
+        return
+
+    def load_employees(self) -> []:
+        data = []
+        try:
+            cmd = 'select id, first_name, last_name, desk_phone, mobile_phone, ' +\
+                'email, title, empid, location, start_date, end_date, image from employees;'
+            c = self._conn.cursor()
+            c = c.execute(cmd)
+            rows = c.fetchall()
+
+            for row in rows:
+                one = {
+                    "id": row[0],
+                    "first_name": row[1],
+                    "last_name": row[2],
+                    "name": row[1] + ' ' + row[2],
+                    "desk_phone": row[3],
+                    "mobile_phone": row[4],
+                    "email": row[5],
+                    "title": row[6],
+                    "empid": row[7],
+                    "location": row[8],
+                    "start_date": row[9],
+                    "end_date": row[10],
+                    "image": row[11],
+                }
+                data.append(one)
+
+        except Error as e:
+            print(e)
+        return data
+
+    def load_employee(self, id) -> []:
+        data = {}
+        try:
+            cmd = 'select id, first_name, last_name, desk_phone, mobile_phone, ' +\
+                'email, title, empid, location, start_date, end_date, image from employees ' +\
+                'where id = ?;'
+            c = self._conn.cursor()
+            c = c.execute(cmd, (id))
+            rows = c.fetchall()
+
+            for row in rows:
+                data = {
+                    "id": row[0],
+                    "first_name": row[1],
+                    "last_name": row[2],
+                    "name": row[1] + ' ' + row[2],
+                    "desk_phone": row[3],
+                    "mobile_phone": row[4],
+                    "email": row[5],
+                    "title": row[6],
+                    "empid": row[7],
+                    "location": row[8],
+                    "start_date": row[9],
+                    "end_date": row[10],
+                    "image": row[11],
+                }
+
+        except Error as e:
+            print(e)
+        return data
+
+    def update_employee(self, data):
+        try:
+            cmd = 'update employees set first_name = ?, last_name = ?, desk_phone = ?, mobile_phone = ?, ' +\
+                'email = ?, title = ?, empid = ?, location = ?, start_date = ?, end_date = ?, image = ? ' +\
+                'where id = ?;'
+            c = self._conn.cursor()
+            params = ( data['first_name'], data['last_name'], data['desk_phone'], data['mobile_phone'],
+                       data['email'], data['title'], data['empid'], data['location'], data['start_date'],
+                       data['end_date'], data['image'], data['id'])
+            c = c.execute(cmd, params)
+            self._commit()
+
+        except Error as e:
+            print(e)
+        return
+
+
+    def create_new_employee(self) -> int:
+        newid = self._get_next_id()
+        self.insert_employee(id=newid, first_name='', last_name='')
+        return newid
